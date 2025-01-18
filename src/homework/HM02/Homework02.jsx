f3b4264c7692d462f111d30de060897758f6af4e
Sergej-L-AIT-Course-2024 <skylerus@mail.de>
2025-01-18 11:01:16 +0100

    update homework2( add person)

--- src/homework/HM02/Homework02.jsx
+++ src/homework/HM02/Homework02.jsx
@@ -1,24 +1,22 @@
 import ProfileCard from "../../assets/components/ProfileCard/ProfileCard.jsx";
 function Homework02() {
   const person1 = {
-    name: "Ivanov Ivan",
+    name: "Ivan Ivanov ",
     profession: "Software Developer",
     hobby:
       " Я увлекаюсь программированием, чтением книг и путешествиями. Люблю изучать новые технологии и создавать интересные проекты.",
     img: "https://klike.net/uploads/posts/2019-03/1551511801_1.jpg",
   };
   const person2 = {
-    name: "Ivanov Ivan",
-    profession: "Software Developer",
-    hobby:
-      " Я увлекаюсь программированием, чтением книг и путешествиями. Люблю изучать новые технологии и создавать интересные проекты.",
+    name: "Anna Smirnova",
+    profession: "QA Engineer",
+    hobby: " я Я увлекаюсь игрой на гитаре. ",
     img: "https://99px.ru/cms/temp/module_temp1/image1_image_12910241918402121245150x150.jpg",
   };
   const person3 = {
-    name: "Ivanov Ivan",
-    profession: "Software Developer",
-    hobby:
-      " Я увлекаюсь программированием, чтением книг и путешествиями. Люблю изучать новые технологии и создавать интересные проекты.",
+    name: " Alex Petrov",
+    profession: "Financial Consultant",
+    hobby: "Фотография дикой природы",
     img: "https://99px.ru/cms/temp/module_temp1/image1_image_1251123173741217343150x150.jpg",
   };
 
