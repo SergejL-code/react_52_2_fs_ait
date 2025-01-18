aacc08072b4ad56caa8dda37f4dd73913c022fc0
Sergej-L-AIT-Course-2024 <skylerus@mail.de>
2025-01-18 10:43:42 +0100

    update Homework.jsx(add  trhee obj),

--- src/assets/components/ProfileCard/ProfileCard.jsx
+++ src/assets/components/ProfileCard/ProfileCard.jsx
@@ -2,18 +2,11 @@ import "./profileCard.css";
 function ProfileCard() {
   return (
     <div className="card">
-      <img
-        className="avatar-img"
-        src="https://klike.net/uploads/posts/2019-03/1551511801_1.jpg"
-        alt=""
-      />
-      <div className="name">Ivanov Ivan</div>
-      <div className="profession">Software Developer</div>
+      <img className="avatar-img" src="" alt="" />
+      <div className="name"></div>
+      <div className="profession"></div>
       <div className="hobby">
-        <p>
-          Я увлекаюсь программированием, чтением книг и путешествиями. Люблю
-          изучать новые технологии и создавать интересные проекты.w
-        </p>
+        <p></p>
       </div>
     </div>
   );
--- src/homework/HM02/Homework02.jsx
+++ src/homework/HM02/Homework02.jsx
@@ -1,5 +1,35 @@
 import ProfileCard from "../../assets/components/ProfileCard/ProfileCard.jsx";
 function Homework02() {
+  const person1 = {
+    name: "Ivanov Ivan",
+    profession: "Software Developer",
+    hobby:
+      " Я увлекаюсь программированием, чтением книг и путешествиями. Люблю изучать новые технологии и создавать интересные проекты.",
+    img: "https://klike.net/uploads/posts/2019-03/1551511801_1.jpg",
+  };
+  const person2 = {
+    name: "Ivanov Ivan",
+    profession: "Software Developer",
+    hobby:
+      " Я увлекаюсь программированием, чтением книг и путешествиями. Люблю изучать новые технологии и создавать интересные проекты.",
+    img: "https://99px.ru/cms/temp/module_temp1/image1_image_12910241918402121245150x150.jpg",
+  };
+  const person3 = {
+    name: "Ivanov Ivan",
+    profession: "Software Developer",
+    hobby:
+      " Я увлекаюсь программированием, чтением книг и путешествиями. Люблю изучать новые технологии и создавать интересные проекты.",
+    img: "https://99px.ru/cms/temp/module_temp1/image1_image_1251123173741217343150x150.jpg",
+  };
+
+  // function showCard(obj) {
+  //   if (obj.name && obj.profession && obj.hobby && obj.img) {
+  //     return `Developer of ${obj.library} is ${obj.developer}!`;
+  //   }
+  //   return "Not valid data 😵";
+  // }
+  //   return;
+  // }
   return (
     <div>
       <ProfileCard />
