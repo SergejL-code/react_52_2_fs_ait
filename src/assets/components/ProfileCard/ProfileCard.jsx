1236907b13419475205782d4c5ba1ce3ad824080
Sergej-L-AIT-Course-2024 <skylerus@mail.de>
2025-01-18 10:05:58 +0100

    update profileCards.css

--- src/assets/components/ProfileCard/ProfileCard.jsx
+++ src/assets/components/ProfileCard/ProfileCard.jsx
@@ -1,11 +1,20 @@
 import "./profileCard.css";
 function ProfileCard() {
   return (
-    <div>
-      <div className="name">d</div>
-      <div className="profession">e</div>
-      <div className="hobby">w</div>
-      <img src='https://klike.net/uploads/posts/2019-03/1551511801_1.jpg} alt=""' />
+    <div className="card">
+      <img
+        className="avatar-img"
+        src="https://klike.net/uploads/posts/2019-03/1551511801_1.jpg"
+        alt=""
+      />
+      <div className="name">Ivanov Ivan</div>
+      <div className="profession">Software Developer</div>
+      <div className="hobby">
+        <p>
+          Я увлекаюсь программированием, чтением книг и путешествиями. Люблю
+          изучать новые технологии и создавать интересные проекты.w
+        </p>
+      </div>
     </div>
   );
 }
