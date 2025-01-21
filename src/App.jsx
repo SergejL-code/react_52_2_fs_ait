589bfd55ef2965813fc075b8d08bae9b5c5cd9a1
Sergej-L-AIT-Course-2024 <skylerus@mail.de>
2025-01-21 10:37:17 +0100

    add component MyButton

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@
 //import reactLogo from "./assets/react.svg";
 //import viteLogo from "/vite.svg";
 import "./App.css";
-import Homework03 from "./homework/HM03/Homework03";
-//import Lesson03 from "./lessons/lesson_03/Lesson03";
+//import Homework03 from "./homework/HM03/Homework03";
+import Lesson03 from "./lessons/lesson_03/Lesson03";
 
 //import Homework02 from "./homework/HM02/Homework02";
 //import Homework_01 from "./homework/HM01/Homework_01";
@@ -37,7 +37,7 @@ import Homework03 from "./homework/HM03/Homework03";
 function App() {
   return (
     <div>
-      <Homework03 />
+      <Lesson03 />
     </div>
   );
 }
--- src/lessons/lesson_03/Lesson03.jsx
+++ src/lessons/lesson_03/Lesson03.jsx
@@ -2,21 +2,30 @@ import MyButton from "../../components/myButton/MyButton";
 import UserCard from "../../components/userCard/UserCard";
 
 function Lesson03() {
-  function sum() {
-    alert(55 + 6);
-  }
-  function open() {
-    window.open("https://de.wikipedia.org/wiki/Wikipedia:Hauptseite");
-  }
+  const handleClick = () => {
+    console.log("Click!");
+  };
+
+  const handleSubmit = () => {
+    alert("Submit");
+  };
   return (
     <div>
       <h2>React Props👨‍👩‍👧‍👦</h2>
+      {/* Вызов компонента */}
       {/* <UserCard name={"Peter"} age={35} hobby={"hiking"} />
       <UserCard name={"Rosa"} age={40} hobby={"diving"} />
       <UserCard name={"Anton"} age={28} /> */}
 
-      <MyButton text={"Berechnen"} funk={sum} />
-      <MyButton text={"Open"} funk={open} />
+      <MyButton func={handleClick} text={"Click me!"} type={"button"} />
+      <MyButton func={handleSubmit} text={"Submit"} type={"submit"} />
+
+      {/* Пример передачи небольшой функций внутри тега вызова без прописного в теле  */}
+      <MyButton
+        func={() => alert("👌 No Handle")}
+        text={"No Handel!"}
+        type={"submit"}
+      />
     </div>
   );
 }
